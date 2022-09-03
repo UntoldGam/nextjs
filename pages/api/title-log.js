bf3b0e45434ccb9e9dc55ced984cbaff51bf0bc1
UntoldGam <93488596+UntoldGam@users.noreply.github.com>
2022-09-03 17:27:36 +0100

    update

---
+++ pages/api/title-log.js
@@ -0,0 +1,30 @@
+const { EmbedBuilder, WebhookClient, inlineCode } = require('discord.js');
+const { titleWebhookId, titleWebhookToken } = process.env;
+
+const titleWebhook = new WebhookClient({ id: titleWebhookId, token: titleWebhookToken });
+
+export default function handler(req, res) {
+  let data = req.query.data;
+  data = JSON.parse(data);
+
+  let title = data.title;
+  let target = data.target;
+  let player = data.player;
+  let action = data.action;
+  console.log(data)
+
+  console.log("properties made")
+  console.log("embed made")
+
+  let embed = new EmbedBuilder();
+  embed.setTitle("Title Log",)
+  embed.setDescription(`Title giver: ${player} \n Title receiver: ${target} \n Title Name: ${title} \n <t:${Math.floor(Date.now() / 1000)}:F>`)
+  embed.setFooter({ "text": `Does this log look incorrect? Contact untold#0830 if this is the case.` })
+  embed.setColor(action === "added" ? 5763719 : 15548997)
+
+  titleWebhook.send({
+    embeds: [embed]
+  });
+  res.status(200).json({ "result": "Log Created and Sent" })
+  //res.status(200).json({ name: 'John Doe' })
+}
