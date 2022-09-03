516ba52f27d1323eb52a0150a6ba806ad1789caa
UntoldGam <93488596+UntoldGam@users.noreply.github.com>
2022-09-03 22:20:42 +0100

    update

--- pages/api/title-log.js
+++ pages/api/title-log.js
@@ -4,19 +4,34 @@ const { titleWebhookId, titleWebhookToken } = process.env;
 const titleWebhook = new WebhookClient({ id: titleWebhookId, token: titleWebhookToken });
 
 export default function handler(req, res) {
-  //let data = req.query.data;
- // data = JSON.parse(data);
-  let data = {
-    "title": "title",
-    "target": "dev",
-    "player": "not dev",
-    "action": "added"
+  let data;
+  if (req.query.debug) {
+    data = {
+      "title": "title",
+      "target": "dev",
+      "player": "not dev",
+      "action": "not added"
+    }
+  } else {
+    data = JSON.parse(req.query.data);
   }
 
+  
   let title = data.title;
   let target = data.target;
   let player = data.player;
   let action = data.action;
+
+
+  let color
+  if (action === "added") {
+    color = 5763719
+  } else if (action === "removed") {
+    color = 15548997
+  } else {
+    res.status(200).send(JSON.parse(`{ \"result\": \"Action is not added or removed: ${action}\" }`))
+  }
+
   console.log(data)
 
   console.log("properties made")
@@ -26,13 +41,11 @@ export default function handler(req, res) {
   embed.setTitle("Title Log",)
   embed.setDescription(`Title giver: ${player} \n Title receiver: ${target} \n Title Name: ${title} \n <t:${Math.floor(Date.now() / 1000)}:F>`)
   embed.setFooter({ "text": `Does this log look incorrect? Contact untold#0830 if this is the case.` })
-  embed.setColor(action === "added" ? 5763719 : 15548997)
+  embed.setColor(color)
 
   titleWebhook.send({
     embeds: [embed]
   }).then(() => {
     res.status(200).json({ "result": "Log Created and Sent" })
   } )
-  
-  //res.status(200).json({ name: 'John Doe' })
 }
