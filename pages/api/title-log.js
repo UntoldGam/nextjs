556f0407902bb22790d266cf79e34ea3a7abc3b6
UntoldGam <93488596+UntoldGam@users.noreply.github.com>
2022-09-03 22:25:13 +0100

    UP!

--- pages/api/title-log.js
+++ pages/api/title-log.js
@@ -4,18 +4,13 @@ const { titleWebhookId, titleWebhookToken } = process.env;
 const titleWebhook = new WebhookClient({ id: titleWebhookId, token: titleWebhookToken });
 
 export default function handler(req, res) {
-  let data;
-  if (!req.query.data) {
-    data = {
-      "title": "Testing_Title",
-      "target": "Testing_Target",
-      "player": "Testing_Player",
-      "action": "Testing_Action"
-    }
-  } else {
-    data = JSON.parse(req.query.data);
+  let data = {
+    "title": "Testing_Title",
+    "target": "Testing_Target",
+    "player": "Testing_Player",
+    "action": "Testing_Action"
   }
-
+  
   
   let title = data.title;
   let target = data.target;
